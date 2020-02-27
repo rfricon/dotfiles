c79b1be988326c3d420f012bb320dfc1b9b9b303
Romain Fricon <romain.fricon@gmail.com>
2020-02-27 21:03:57 +0100

    add fish config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+# # Set up Node Version Manager
+# set -x NVM_DIR "$HOME/.nvm"                            # You can change this if you want.
+# set -x NVM_SOURCE "/usr/share/nvm"                     # The AUR package installs it to here.
+# [ -s "$NVM_SOURCE/nvm.sh" ] && bass . "$NVM_SOURCE/nvm.sh"  # Load NVM
+
+# bass source /usr/share/nvm/init-nvm.sh
+
+test -s /home/romain/.nvm/nvm.fish; and source /home/romain/.nvm/nvm.fish
